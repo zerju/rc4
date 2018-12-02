b758bd9fb81afb84a8fe784709d609629e3c0bf3
zerju <jure.zerak@gmail.com>
2018-12-02 11:14:54 +0100

    rc4 c# code

---
+++ rc4/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace rc4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selFile = new System.Windows.Forms.Button();
+            this.encKey = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.encryptBtn = new System.Windows.Forms.Button();
+            this.decryptBtn = new System.Windows.Forms.Button();
+            this.genKey = new System.Windows.Forms.Button();
+            this.saveKey = new System.Windows.Forms.Button();
+            this.importKey = new System.Windows.Forms.Button();
+            this.faultyKeyText = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.encSpeed = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.decSpeed = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numForKey = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // selFile
+            // 
+            this.selFile.Location = new System.Drawing.Point(13, 13);
+            this.selFile.Name = "selFile";
+            this.selFile.Size = new System.Drawing.Size(75, 23);
+            this.selFile.TabIndex = 0;
+            this.selFile.Text = "Select file...";
+            this.selFile.UseVisualStyleBackColor = true;
+            this.selFile.Click += new System.EventHandler(this.selFile_Click);
+            // 
+            // encKey
+            // 
+            this.encKey.Location = new System.Drawing.Point(44, 49);
+            this.encKey.Name = "encKey";
+            this.encKey.Size = new System.Drawing.Size(287, 20);
+            this.encKey.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Key:";
+            // 
+            // encryptBtn
+            // 
+            this.encryptBtn.Location = new System.Drawing.Point(12, 260);
+            this.encryptBtn.Name = "encryptBtn";
+            this.encryptBtn.Size = new System.Drawing.Size(75, 23);
+            this.encryptBtn.TabIndex = 3;
+            this.encryptBtn.Text = "Encrypt";
+            this.encryptBtn.UseVisualStyleBackColor = true;
+            this.encryptBtn.Click += new System.EventHandler(this.encryptBtn_Click);
+            // 
+            // decryptBtn
+            // 
+            this.decryptBtn.Location = new System.Drawing.Point(256, 260);
+            this.decryptBtn.Name = "decryptBtn";
+            this.decryptBtn.Size = new System.Drawing.Size(75, 23);
+            this.decryptBtn.TabIndex = 4;
+            this.decryptBtn.Text = "Decrypt";
+            this.decryptBtn.UseVisualStyleBackColor = true;
+            this.decryptBtn.Click += new System.EventHandler(this.decryptBtn_Click);
+            // 
+            // genKey
+            // 
+            this.genKey.Location = new System.Drawing.Point(19, 121);
+            this.genKey.Name = "genKey";
+            this.genKey.Size = new System.Drawing.Size(103, 23);
+            this.genKey.TabIndex = 5;
+            this.genKey.Text = "Generate Key";
+            this.genKey.UseVisualStyleBackColor = true;
+            this.genKey.Click += new System.EventHandler(this.genKey_Click);
+            // 
+            // saveKey
+            // 
+            this.saveKey.Location = new System.Drawing.Point(256, 121);
+            this.saveKey.Name = "saveKey";
+            this.saveKey.Size = new System.Drawing.Size(75, 23);
+            this.saveKey.TabIndex = 6;
+            this.saveKey.Text = "Save Key";
+            this.saveKey.UseVisualStyleBackColor = true;
+            this.saveKey.Click += new System.EventHandler(this.saveKey_Click);
+            // 
+            // importKey
+            // 
+            this.importKey.Location = new System.Drawing.Point(142, 121);
+            this.importKey.Name = "importKey";
+            this.importKey.Size = new System.Drawing.Size(75, 23);
+            this.importKey.TabIndex = 7;
+            this.importKey.Text = "Import key";
+            this.importKey.UseVisualStyleBackColor = true;
+            this.importKey.Click += new System.EventHandler(this.importKey_Click);
+            // 
+            // faultyKeyText
+            // 
+            this.faultyKeyText.AutoSize = true;
+            this.faultyKeyText.Location = new System.Drawing.Point(16, 147);
+            this.faultyKeyText.Name = "faultyKeyText";
+            this.faultyKeyText.Size = new System.Drawing.Size(35, 13);
+            this.faultyKeyText.TabIndex = 8;
+            this.faultyKeyText.Text = "label2";
+            this.faultyKeyText.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Encryption speed:";
+            // 
+            // encSpeed
+            // 
+            this.encSpeed.AutoSize = true;
+            this.encSpeed.Location = new System.Drawing.Point(114, 191);
+            this.encSpeed.Name = "encSpeed";
+            this.encSpeed.Size = new System.Drawing.Size(35, 13);
+            this.encSpeed.TabIndex = 10;
+            this.encSpeed.Text = "label3";
+            this.encSpeed.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 217);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Decryption speed:";
+            // 
+            // decSpeed
+            // 
+            this.decSpeed.AutoSize = true;
+            this.decSpeed.Location = new System.Drawing.Point(114, 217);
+            this.decSpeed.Name = "decSpeed";
+            this.decSpeed.Size = new System.Drawing.Size(35, 13);
+            this.decSpeed.TabIndex = 12;
+            this.decSpeed.Text = "label4";
+            this.decSpeed.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(171, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Number of bytes in key generation:";
+            // 
+            // numForKey
+            // 
+            this.numForKey.Location = new System.Drawing.Point(193, 83);
+            this.numForKey.Name = "numForKey";
+            this.numForKey.Size = new System.Drawing.Size(36, 20);
+            this.numForKey.TabIndex = 14;
+            this.numForKey.Text = "16";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 295);
+            this.Controls.Add(this.numForKey);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.decSpeed);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.encSpeed);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.faultyKeyText);
+            this.Controls.Add(this.importKey);
+            this.Controls.Add(this.saveKey);
+            this.Controls.Add(this.genKey);
+            this.Controls.Add(this.decryptBtn);
+            this.Controls.Add(this.encryptBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.encKey);
+            this.Controls.Add(this.selFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button selFile;
+        private System.Windows.Forms.TextBox encKey;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button encryptBtn;
+        private System.Windows.Forms.Button decryptBtn;
+        private System.Windows.Forms.Button genKey;
+        private System.Windows.Forms.Button saveKey;
+        private System.Windows.Forms.Button importKey;
+        private System.Windows.Forms.Label faultyKeyText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label encSpeed;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label decSpeed;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox numForKey;
+    }
+}
+
---
+++ rc4/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rc4
+{
+    public partial class Form1 : Form
+    {
+        byte[] fileBytes;
+        string filename;
+        string keyFile;
+        string keyData;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void selFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "All Files|*";
+            openFileDialog1.Title = "Select a File";
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+               filename = openFileDialog1.FileName;
+                fileBytes = System.IO.File.ReadAllBytes(@filename);
+            }
+        }
+
+        private void importKey_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Text Files|* txt";
+            openFileDialog1.Title = "Select a Text File";
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                keyFile = openFileDialog1.FileName;
+                keyData = System.IO.File.ReadAllText(@keyFile);
+                byte[] keyBytes = Encoding.Unicode.GetBytes(keyData);
+                if(keyBytes.Length < 1)
+                {
+                    faultyKeyText.Text = "The key is too short, it should be between 1 and 256 bytes";
+                    faultyKeyText.Visible = true;
+                } else if (keyBytes.Length > 256)
+                {
+                    faultyKeyText.Text = "The key is too long, it should be between 1 and 256 bytes";
+                    faultyKeyText.Visible = true;
+                } else
+                {
+                    faultyKeyText.Visible = false;
+                    encKey.Text = keyData;
+                }
+            }
+        }
+        private void saveKey_Click(object sender, EventArgs e)
+        {
+            string keyText = encKey.Text;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.InitialDirectory = @Environment.GetFolderPath(Environment.SpecialFolder.Desktop);  
+            saveFileDialog1.Title = "Save text Files";
+            saveFileDialog1.CheckFileExists = false;
+            saveFileDialog1.CheckPathExists = true;
+            saveFileDialog1.DefaultExt = "txt";
+            saveFileDialog1.Filter = "Text files (*.txt)|*.txt";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (saveFileDialog1.FileName != "")
+                {
+                    System.IO.File.WriteAllText(saveFileDialog1.FileName, keyText);
+                }
+            }
+
+        }
+
+
+        private void genKey_Click(object sender, EventArgs e)
+        {
+            bool isNumeric = int.TryParse(numForKey.Text, out int n);
+            if (isNumeric) { 
+                encKey.Text = getUniqueKey(Int32.Parse(numForKey.Text));
+            }
+        }
+
+        private void encryptBtn_Click(object sender, EventArgs e)
+        {
+            Stopwatch sw = new Stopwatch();
+
+            byte[] keyBytes = Encoding.Unicode.GetBytes(encKey.Text);
+            if (keyBytes.Length < 1)
+            {
+                faultyKeyText.Text = "The key is too short, it should be between 1 and 256 bytes";
+                faultyKeyText.Visible = true;
+            }
+            else if (keyBytes.Length > 256)
+            {
+                faultyKeyText.Text = "The key is too long, it should be between 1 and 256 bytes";
+                faultyKeyText.Visible = true;
+            }
+            else
+            {
+                sw.Start();
+
+                faultyKeyText.Visible = false;
+                byte[] encryptedData = encrypt(keyBytes, fileBytes);
+                System.IO.File.WriteAllBytes(filename + ".enc", encryptedData);
+
+                sw.Stop();
+                
+                long seconds = sw.ElapsedMilliseconds * 1000;
+                long speed = fileBytes.Length / seconds;
+
+                encSpeed.Text = speed + " B/s";
+                encSpeed.Visible = true;
+
+            }
+            
+        }
+
+
+
+        private void decryptBtn_Click(object sender, EventArgs e)
+        {
+            Stopwatch sw = new Stopwatch();
+            byte[] key = Encoding.Unicode.GetBytes(encKey.Text);
+            if (key.Length < 1)
+            {
+                faultyKeyText.Text = "The key is too short, it should be between 1 and 256 bytes";
+                faultyKeyText.Visible = true;
+            }
+            else if (key.Length > 256)
+            {
+                faultyKeyText.Text = "The key is too long, it should be between 1 and 256 bytes";
+                faultyKeyText.Visible = true;
+            }
+            else
+            {
+                sw.Start();
+
+                faultyKeyText.Visible = false;
+                byte[] decryptedData = decrypt(key, fileBytes);
+                int index = filename.LastIndexOf(".");
+                if (index > 0)
+                {
+                    filename = filename.Substring(0, index);
+                }
+                string tempFilename;
+                int index2 = filename.LastIndexOf(".");
+                if (index2 > 0)
+                {
+                    tempFilename = filename.Substring(0, index2);
+                    tempFilename = tempFilename + "Decrypted" + filename.Substring(index2);
+                    filename = tempFilename;
+                }
+                System.IO.File.WriteAllBytes(filename, decryptedData);
+                sw.Stop();
+
+                long seconds = sw.ElapsedMilliseconds * 1000;
+                long speed = fileBytes.Length / seconds;
+
+                decSpeed.Text = speed + " B/s";
+                decSpeed.Visible = true;
+            }
+           
+        }
+
+        private byte[] encrypt(byte[] key, byte[] encryptionData)
+        {
+            byte[] encrypted = new byte[encryptionData.Length];
+            byte[] initKey = initializePermutation(key);
+
+            int i = 0;
+            int j = 0;
+
+            for(int b = 0; b < encryptionData.Length; b++)
+            {
+                i = (i + 1) % 255;
+                j = (j + initKey[i]) % 255;
+
+                byte c = initKey[i];
+                initKey[i] = initKey[j];
+                initKey[j] = c;
+
+                encrypted[b] = (byte)(encryptionData[b] ^ initKey[(initKey[i] + initKey[j]) % 255]);
+            }
+            return encrypted;
+        }
+
+        private byte[] decrypt(byte[] key, byte[] decrpyptionData)
+        {
+
+            byte[] decrypted = new byte[decrpyptionData.Length];
+            byte[] initKey = initializePermutation(key);
+
+            int i = 0;
+            int j = 0;
+
+            for (int b = 0; b < decrpyptionData.Length; b++)
+            {
+                i = (i + 1) % 255;
+                j = (j + initKey[i]) % 255;
+
+                byte c = initKey[i];
+                initKey[i] = initKey[j];
+                initKey[j] = c;
+
+                decrypted[b] = (byte)(decrpyptionData[b] ^ initKey[(initKey[i] + initKey[j]) % 255]);
+            }
+            return decrypted;
+        }
+
+        private byte[] initializePermutation(byte[] key)
+        {
+            byte[] s = new byte[256];
+            for(int i = 0; i < 256; i++)
+            {
+                s[i] = (byte)i;
+            }
+            for (int i = 0, j = 0; i < 256; i++)
+            {
+                
+                j = (j + s[i] + key[i % key.Length]) % 255;
+
+                byte c = s[i];
+                s[i] = s[j];
+                s[j] = c;
+            }
+
+            return s;
+        }
+
+        public string getUniqueKey(int size)
+        {
+            char[] chars =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".ToCharArray();
+            byte[] data = new byte[size];
+            using (RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider())
+            {
+                crypto.GetBytes(data);
+            }
+            StringBuilder result = new StringBuilder(size);
+            foreach (byte b in data)
+            {
+                result.Append(chars[b % (chars.Length)]);
+            }
+            return result.ToString();
+        }
+
+    }
+}
